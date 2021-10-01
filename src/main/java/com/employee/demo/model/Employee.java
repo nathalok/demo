5cd913896031c74b4b7bb3345d52b39433f524ec
Alok Nath <alok.nath@agersens.com>
2021-10-02 09:17:59 +1000

    Get Employees API done

---
+++ src/main/java/com/employee/demo/model/Employee.java
@@ -0,0 +1,61 @@
+package com.employee.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name="email_id")
+    private String email_id;
+
+    public Employee() {}
+
+    public Employee(Long id, String first_name, String last_name, String email_id) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email_id = email_id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail_id() {
+        return email_id;
+    }
+
+    public void setEmail_id(String email_id) {
+        this.email_id = email_id;
+    }
+}
